54c5893cd57b49bd83307e6cf6a28458c4c630d1
AhmedMohamedAshour2025 <shamaahmed569@gmail.com>
2023-07-21 12:23:35 +0300

    change theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const Noteapp());
 }
+
 class Noteapp extends StatelessWidget {
   const Noteapp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: Scaffold(appBar: AppBar(backgroundColor: Colors.white,),));
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light(),
+        home: Scaffold());
   }
 }
-
-
