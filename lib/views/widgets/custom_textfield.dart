e7757aa494e9709fa1e8b596defc18bf0c694235
AhmedMohamedAshour2025 <shamaahmed569@gmail.com>
2023-07-29 15:15:22 +0300

    create custom TextField

---
+++ lib/views/widgets/custom_textfield.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/constant.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField(
+      {required this.hint, this.maxlines = 1, Key? key, this.onSaved})
+      : super(key: key);
+  final String hint;
+  final int maxlines;
+  final void Function(String?)? onSaved;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onSaved: onSaved,
+      validator: (value) {
+        if (value?.isEmpty ?? true) {
+          return 'field is required';
+        } else {
+          return null;
+        }
+      },
+      maxLines: maxlines,
+      cursorColor: kprimaryColor,
+      decoration: InputDecoration(
+          hintText: hint,
+          border: buildBorder(),
+          enabledBorder: buildBorder(),
+          focusedBorder: buildBorder(kprimaryColor)),
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide(color: color ?? Colors.white));
+  }
+}
