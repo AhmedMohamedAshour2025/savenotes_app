09b6bc511b31527e39e41984fc0de4a3bc1755ac
AhmedMohamedAshour2025 <shamaahmed569@gmail.com>
2023-07-29 23:32:28 +0300

    add note cubit extends cubit

---
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -0,0 +1,12 @@
+import 'package:finalapp/models/note_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part  'add_note_state.dart';
+
+//import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AddNoteCubit extends Cubit<AddNoteState>{
+  AddNoteCubit(super.initialState);
+  addnote(NoteModel note){}
+
+}
