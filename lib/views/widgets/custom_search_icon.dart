73c9fc825f068efefe4f581e695157cb7e616c17
AhmedMohamedAshour2025 <shamaahmed569@gmail.com>
2023-07-29 15:16:57 +0300

    create custom icon (search,check)

---
+++ lib/views/widgets/custom_search_icon.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomIconSearch extends StatelessWidget {
+  const CustomIconSearch({Key? key, required this.icon}) : super(key: key);
+  final IconData icon;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 46,
+      width: 46,
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.05),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Icon(
+        icon,
+        size: 28,
+      ),
+    );
+  }
+}
