cea3b0a99f83afe68414213ed2b323a9e4fcd6e0
AhmedMohamedAshour2025 <shamaahmed569@gmail.com>
2023-07-29 15:37:52 +0300

    change the project theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
-import 'package:flutter/material.dart';
+import 'package:finalapp/views/notes_view.dart';
 
-void main() {
-  runApp(const Noteapp());
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+
+import 'constants/constant.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
+  runApp(const NoteApp());
 }
 
-class Noteapp extends StatelessWidget {
-  const Noteapp({Key? key}) : super(key: key);
+class NoteApp extends StatelessWidget {
+  const NoteApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        theme: ThemeData.light(),
-        home: Scaffold());
+        theme: ThemeData(brightness: Brightness.dark),
+        home: const NotesVeiw());
   }
 }
