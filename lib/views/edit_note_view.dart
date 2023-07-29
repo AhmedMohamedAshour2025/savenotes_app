a5132ddf7aba9200a18106f18210da4e7539e23d
AhmedMohamedAshour2025 <shamaahmed569@gmail.com>
2023-07-29 15:30:37 +0300

    create edit note view shows when click notes item

---
+++ lib/views/edit_note_view.dart
@@ -0,0 +1,12 @@
+
+import 'package:finalapp/views/widgets/edit_note_view_body.dart';
+import 'package:flutter/material.dart';
+
+class EditNoteView extends StatelessWidget {
+  const EditNoteView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: EditNoteViewBody(),);
+  }
+}
