f351298a7c868321752d4bc2040a214efee468bd
AhmedMohamedAshour2025 <shamaahmed569@gmail.com>
2023-07-31 15:25:02 +0300

    add CircularProgressIndicator just click AddButton

--- lib/views/widgets/add_note_button.dart
+++ lib/views/widgets/add_note_button.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 import '../../constants/constant.dart';
 
 class AddNoteButton extends StatelessWidget {
-  const AddNoteButton({Key? key, this.onTap}) : super(key: key);
+  const AddNoteButton({Key? key, this.onTap, required this.isLoading}) : super(key: key);
   final void Function()? onTap;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -14,7 +15,7 @@ class AddNoteButton extends StatelessWidget {
         alignment: Alignment.center,
         decoration: BoxDecoration(
             color: kprimaryColor, borderRadius: BorderRadius.circular(8)),
-        child: const Text(
+        child:isLoading ? const CircularProgressIndicator(color: Colors.black,): const Text(
           'Add',
           style: TextStyle(color: Colors.black, fontSize: 24),
         ),
