493592ea8df14099d0c1d74695ab439e9b34f121
AhmedMohamedAshour2025 <shamaahmed569@gmail.com>
2023-08-01 15:26:21 +0300

    display NoteModel date on NotesItem

--- lib/views/item_listview.dart
+++ lib/views/item_listview.dart
@@ -1,7 +1,9 @@
-
-
+import 'package:finalapp/cubits/notes_cubit/notes_cubit.dart';
+import 'package:finalapp/models/note_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../cubits/notes_cubit/notes_state.dart';
 import 'widgets/notes_item.dart';
 
 class ItemListView extends StatelessWidget {
@@ -9,11 +11,19 @@ class ItemListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding:  EdgeInsets.only(bottom: 16),
-          child:  NotesItem(),
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        List<NoteModel> notes =
+            BlocProvider.of<NotesCubit>(context).notes ?? [];
+        print(notes.length);
+        return ListView.builder(
+          itemCount: notes.length,
+          itemBuilder: (context, index) {
+            return  Padding(
+              padding:const EdgeInsets.only(bottom: 16),
+              child: NotesItem(note: notes[index],),
+            );
+          },
         );
       },
     );
